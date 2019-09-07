8a878bf8300c5a461bfbbce1600998964aa36758
leejoo <40150501@qq.com>
2019-09-07 17:19:51 +0800

    增加登录验证

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,15 @@
 
 Route::group(['prefix'=>'admin'], function () {
 	//后台登录页面
-	Route::get('public/login','Admin\PublicController@login');
+	Route::get('public/login','Admin\PublicController@login')->name('login');
+
 	//后台登录处理
     Route::post('public/login','Admin\PublicController@login');
+});
+
+//需要登录验证的
+Route::group(['prefix'=>'admin','middleware' => 'auth:admin'], function () {
+	//后台首页
 	Route::get('index/index','Admin\IndexController@index');
 	Route::get('index/welcome','Admin\IndexController@welcome');
 
